2e124094daf17c488adbdf559ae1a4bd51b4fc50
Xavi <mrjavoman@hotmail.com>
2017-02-26 15:25:55 -0600

    Adding files up to mid section quiz for section 5

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+    public LevelManager levelManger;
+
+
+    void OnTriggerEnter2D(Collider2D trigger)
+    {
+        print("Trigger");
+        levelManger.LoadLevel("Win");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision");
+    }
+}
